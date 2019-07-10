635f8e629eaeb4e1d0c9d8a9484d160a2520cbee
Phillmont Muktar <phillmont@u.nus.edu>
2019-07-10 20:17:21 +0700

    Added toast and code cleanup

--- bookmarklet.js
+++ bookmarklet.js
@@ -17,13 +17,46 @@
     const filename = document.title;
     const link = document.createElement("a");
 
+    // For browsers that support HTML5 download
     if (typeof link.download !== typeof undefined) {
         link.href = media;
         link.download = filename;
-        link.title = "Download by BBBB";
         document.body.appendChild(link);
         link.dispatchEvent(new MouseEvent('click'));
+        document.body.removeChild(link);
     } else {
-        window.open(media, '_blank');
+        // If not, too bad :(
+        window.open(media, '_self');
     }
+
+    let toast = createToast(filename);
+
+    // After 3s, remove the toast
+    setTimeout(function () {
+        document.body.removeChild(toast);
+    }, 3000);
+
+    // Remove the added launch script
+    document.body.removeChild(document.getElementById("PanoptoBookmarklet"));
 })();
+
+function createToast(text) {
+    const toast = document.createElement("div");
+    toast.innerHTML = `<b>${text}</b> is now downloading`;
+    toast.style.minWidth = "250px";
+    toast.style.marginLeft = "-125px";
+    toast.style.backgroundColor = "#2c3e50";
+    toast.style.color = "white";
+    toast.style.textAlign = "center";
+    toast.style.borderRadius = "7px";
+    toast.style.padding = "16px";
+    toast.style.fontSize = "14px";
+    toast.style.position = "fixed";
+    toast.style.zIndex = "1";
+    toast.style.left = "50%";
+    toast.style.bottom = "30px";
+    toast.style.boxShadow = "0px 25px 40px -10px rgba(0,0,0,0.8)";
+    toast.style.webkitBoxShadow = "0px 25px 40px -10px rgba(0,0,0,0.8)";
+    document.body.appendChild(toast);
+    return toast;
+}
